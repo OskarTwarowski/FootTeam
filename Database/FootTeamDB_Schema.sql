aaa99aae8d2ce980379b53ccf402fa55a3b6522e
KamilZakolski <zakolski@outlook.com>
2025-11-02 13:50:35 +0100

    Dodano tabelę powiadomień

--- Database/FootTeamDB_Schema.sql
+++ Database/FootTeamDB_Schema.sql
@@ -6,7 +6,7 @@ USE FootTeamDB;
 -- Tabela użytkowników
 CREATE TABLE Users (
     UserID INT AUTO_INCREMENT PRIMARY KEY,
-    Username VARCHAR(50) NOT NULL UNIQUE,
+    --Username VARCHAR(50) NOT NULL UNIQUE,
     Email VARCHAR(100) NOT NULL UNIQUE,
     PasswordHash VARCHAR(255) NOT NULL,
     Role ENUM('Admin', 'Coach', 'Parent') NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE Users (
 CREATE TABLE Teams (
     TeamID INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
-    Category VARCHAR(50),
+    --Category VARCHAR(50),
     CoachID INT,
     FOREIGN KEY (CoachID) REFERENCES Users(UserID) ON DELETE SET NULL
 );
@@ -27,15 +27,15 @@ CREATE TABLE Players (
     PlayerID INT AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
-    BirthDate DATE,
-    Position VARCHAR(50),
+    --BirthDate DATE,
+    --Position VARCHAR(50),
     TeamID INT,
     UserID INT,
     FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE SET NULL,
     FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE SET NULL
 );
 
--- Tabela treningów
+-- Tabela treningów / events
 CREATE TABLE Trainings (
     TrainingID INT AUTO_INCREMENT PRIMARY KEY,
     Title VARCHAR(100),
@@ -49,22 +49,19 @@ CREATE TABLE Trainings (
     FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE SET NULL
 );
 
--- Tabela przypisania zawodników do treningów
-CREATE TABLE TrainingParticipants (
-    TrainingID INT,
-    PlayerID INT,
-    PRIMARY KEY (TrainingID, PlayerID),
-    FOREIGN KEY (TrainingID) REFERENCES Trainings(TrainingID) ON DELETE CASCADE,
-    FOREIGN KEY (PlayerID) REFERENCES Players(PlayerID) ON DELETE CASCADE
-);
-
--- Tabela wydarzeń
+-- Tabela powiadomień LOG
 CREATE TABLE Events (
     EventID INT AUTO_INCREMENT PRIMARY KEY,
     Title VARCHAR(100),
     Description TEXT,
     EventDate DATETIME,
-    Location VARCHAR(100),
+    --Location VARCHAR(100),
     CreatedBy INT,
     FOREIGN KEY (CreatedBy) REFERENCES Users(UserID) ON DELETE SET NULL
+    --TeamID
 );
+
+-- Payments
+CREATE TABLE Payments (
+    UserID
+)
