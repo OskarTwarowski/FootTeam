8d2b77f9c894a2ee281e3d1eb35f2e4b3ea1b8bd
KamilZakolski <zakolski@outlook.com>
2025-11-02 13:51:07 +0100

    Dodano tabelę powiadomień

--- Database/FootTeamDB_Schema.sql
+++ Database/FootTeamDB_Schema.sql
@@ -1,46 +1,36 @@
 
--- Tworzenie bazy danych
 CREATE DATABASE IF NOT EXISTS FootTeamDB;
 USE FootTeamDB;
 
--- Tabela użytkowników
 CREATE TABLE Users (
     UserID INT AUTO_INCREMENT PRIMARY KEY,
-    --Username VARCHAR(50) NOT NULL UNIQUE,
     Email VARCHAR(100) NOT NULL UNIQUE,
     PasswordHash VARCHAR(255) NOT NULL,
     Role ENUM('Admin', 'Coach', 'Parent') NOT NULL,
     CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
--- Tabela drużyn
 CREATE TABLE Teams (
     TeamID INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
-    --Category VARCHAR(50),
     CoachID INT,
     FOREIGN KEY (CoachID) REFERENCES Users(UserID) ON DELETE SET NULL
 );
 
--- Tabela zawodników
 CREATE TABLE Players (
     PlayerID INT AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
-    --BirthDate DATE,
-    --Position VARCHAR(50),
     TeamID INT,
     UserID INT,
     FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE SET NULL,
     FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE SET NULL
 );
 
--- Tabela treningów / events
 CREATE TABLE Trainings (
     TrainingID INT AUTO_INCREMENT PRIMARY KEY,
     Title VARCHAR(100),
     Description TEXT,
-    --Location VARCHAR(100),
     StartTime DATETIME,
     EndTime DATETIME,
     CoachID INT,
@@ -49,19 +39,13 @@ CREATE TABLE Trainings (
     FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE SET NULL
 );
 
--- Tabela powiadomień LOG
-CREATE TABLE Events (
-    EventID INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Notifications (
+    NotificationID INT AUTO_INCREMENT PRIMARY KEY,
     Title VARCHAR(100),
     Description TEXT,
-    EventDate DATETIME,
-    --Location VARCHAR(100),
+    StartTime DATETIME,
+    EndTime DATETIME,
     CreatedBy INT,
     FOREIGN KEY (CreatedBy) REFERENCES Users(UserID) ON DELETE SET NULL
-    --TeamID
+    FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE SET NULL
 );
-
--- Payments
-CREATE TABLE Payments (
-    UserID
-)
