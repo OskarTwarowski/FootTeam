63b14418423cdbc669da2c43b1825fc7089c1c17
KamilZakolski <zakolski@outlook.com>
2025-11-02 13:52:31 +0100

    Dodano tabelę powiadomień

--- Database/FootTeamDB_Schema.sql
+++ Database/FootTeamDB_Schema.sql
@@ -1,12 +1,9 @@
-
--- Tworzenie bazy danych
 CREATE DATABASE IF NOT EXISTS FootTeamDB;
 USE FootTeamDB;
 
 CREATE TABLE Users (
     UserID INT AUTO_INCREMENT PRIMARY KEY,
     Email VARCHAR(100) NOT NULL UNIQUE,
-    Phone VARCHAR(20),
     PasswordHash VARCHAR(255) NOT NULL,
     Role ENUM('Admin', 'Coach', 'Parent') NOT NULL,
     CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
@@ -23,8 +20,6 @@ CREATE TABLE Players (
     PlayerID INT AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
-    BirthDate DATE,
-    Position VARCHAR(50),
     TeamID INT,
     UserID INT,
     FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE SET NULL,
@@ -43,22 +38,13 @@ CREATE TABLE Trainings (
     FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE SET NULL
 );
 
--- Tabela przypisania zawodników do treningów
-CREATE TABLE TrainingParticipants (
-    TrainingID INT,
-    PlayerID INT,
-    PRIMARY KEY (TrainingID, PlayerID),
-    FOREIGN KEY (TrainingID) REFERENCES Trainings(TrainingID) ON DELETE CASCADE,
-    FOREIGN KEY (PlayerID) REFERENCES Players(PlayerID) ON DELETE CASCADE
-);
-
--- Tabela wydarzeń
-CREATE TABLE Events (
-    EventID INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Notifications (
+    NotificationID INT AUTO_INCREMENT PRIMARY KEY,
     Title VARCHAR(100),
     Description TEXT,
-    EventDate DATETIME,
-    Location VARCHAR(100),
+    StartTime DATETIME,
+    EndTime DATETIME,
     CreatedBy INT,
     FOREIGN KEY (CreatedBy) REFERENCES Users(UserID) ON DELETE SET NULL
+    FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE SET NULL
 );
