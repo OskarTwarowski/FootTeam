7d9b3fad79d47088bda38d08b3528902876e4afd
OskarTwarowski <134192444+OskarTwarowski@users.noreply.github.com>
2025-12-10 21:03:31 +0100

    api

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5100/api",
+  baseURL: "https://footteam-production.up.railway.app/api",
 });
 
 API.interceptors.request.use((config) => {
