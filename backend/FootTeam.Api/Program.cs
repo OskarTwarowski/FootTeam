1ad4880ce2a454318b1baef059be820d902afec5
OskarTwarowski <134192444+OskarTwarowski@users.noreply.github.com>
2025-12-16 15:34:55 +0100

    cors2

--- backend/FootTeam.Api/Program.cs
+++ backend/FootTeam.Api/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+Console.WriteLine(">>> FOOTTEAM BACKEND STARTED <<<");
 
 // Configure CORS
 // builder.Services.AddCors(options =>
