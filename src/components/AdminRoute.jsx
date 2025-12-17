33d468ec93477973c0fc9e92b0860a4263a127da
OskarTwarowski <134192444+OskarTwarowski@users.noreply.github.com>
2025-12-17 22:27:05 +0100

    admin

--- src/components/AdminRoute.jsx
+++ src/components/AdminRoute.jsx
@@ -2,11 +2,11 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function AdminRoute({ children }) {
-  const user = useSelector((state) => state.auth.user);
+  const user = useSelector((state) => state?.auth?.user);
 
   if (!user) return null;
 
-  if (user.role !== "Admin") {
+  if (user?.role !== "Admin") {
     return <Navigate to="/app/profil" replace />;
   }
 
